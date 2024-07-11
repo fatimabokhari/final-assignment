6c5136a33b9cad48ee59871c571451a22ebf68d5
fatimabokhari <100520831+fatimabokhari@users.noreply.github.com>
2024-07-11 19:59:16 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@
 provider "aws" {
   region = "us-east-1"  # Replace with your desired AWS region
 }
-
 # VPC resource
 resource "aws_vpc" "example_vpc" {
   cidr_block = "10.0.0.0/16"
@@ -50,6 +49,53 @@ resource "aws_route_table_association" "example_route_association" {
   route_table_id = aws_route_table.example_route_table.id
 }
 
+# # VPC resource
+# resource "aws_vpc" "example_vpc" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "ExampleVPC"
+#   }
+# }
+
+# # Subnet resource
+# resource "aws_subnet" "example_subnet" {
+#   vpc_id            = aws_vpc.example_vpc.id
+#   cidr_block        = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "ExampleSubnet"
+#   }
+# }
+
+# # Internet Gateway resource
+# resource "aws_internet_gateway" "example_igw" {
+#   vpc_id = aws_vpc.example_vpc.id
+
+#   tags = {
+#     Name = "ExampleIGW"
+#   }
+# }
+
+# # Route Table resource
+# resource "aws_route_table" "example_route_table" {
+#   vpc_id = aws_vpc.example_vpc.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.example_igw.id
+#   }
+
+#   tags = {
+#     Name = "ExampleRouteTable"
+#   }
+# }
+
+# # Route Table Association resource
+# resource "aws_route_table_association" "example_route_association" {
+#   subnet_id      = aws_subnet.example_subnet.id
+#   route_table_id = aws_route_table.example_route_table.id
+# }
+
 # Security Group resource
 resource "aws_security_group" "example_security_group" {
   name   = "ExampleSecurityGroup"
@@ -87,10 +133,15 @@ resource "aws_instance" "example_instance" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.example_subnet.id
   key_name      = "MyKeyPair"  # Replace with your SSH key pair name
+  associate_public_ip_address=true
 
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
 
   tags = {
     Name = "ExampleInstance"
   }
+
+  # Ansible provisioner to run playbook after instance creation
+ 
 }
+
